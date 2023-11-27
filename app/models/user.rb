d90277509d32695274bacef1f3e9feed65b64a8b
yura1128 <yumeto1128.pr@gmail.com>
2023-11-27 18:05:01 +0900

    コメント修正後２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :encrypted_password, :firstname_kanji, :lastname_kanji, :firstname_kana, :lastname_kana, :birthday, presence: true
+  validates :nickname, :firstname_kanji, :lastname_kanji, :firstname_kana, :lastname_kana, :birthday, presence: true
 
   validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must contain @ symbol" }
-  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/, message: "must contain both letters and numbers" }
-  validates :password_confirmation, presence: true
-  validates :lastname_kanji, :firstname_kanji, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/, message: "must be full-width (Kanji, Hiragana, Katakana)" }
-  validates :lastname_kana, :firstname_kana, presence: true, format: { with: /\A[\p{Katakana}]+\z/, message: "must be full-width Katakana" }
+  validates :password, presence: true, length: { minimum: 6 }
+  validate  :password_complexity
+  validates :lastname_kanji, :firstname_kanji, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/, message: "must be full-width (Kanji, Hiragana, Katakana)" }
+  validates :lastname_kana, :firstname_kana, format: { with: /\A[\p{Katakana}]+\z/, message: "must be full-width Katakana" }
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    unless password.match?(/^(?=.*[a-zA-Z])(?=.*\d)/)
+      errors.add(:password, 'must contain both letters and numbers')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
+
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
     context '正常系' do
       it '全ての項目が正しく入力されている場合、有効であること' do
-        user = FactoryBot.build(:user)
-        expect(user).to be_valid
+        expect(@user).to be_valid
       end
     end
 
     context '異常系' do
-      before do
-        @user = FactoryBot.build(:user)
-      end
-
       it 'ニックネームが空では登録できない' do
         @user.nickname = ''
         @user.valid?
@@ -52,9 +52,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
       end
 
-      it 'パスワードが半角英数字混合でない場合は登録できない' do
+      it 'パスワードが半角数字のみの場合は登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password must contain both letters and numbers"
+      end
+
+      it 'パスワードが半角英字のみの場合は登録できない' do
         @user.password = 'password'
         @user.password_confirmation = 'password'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password must contain both letters and numbers"
+      end
+
+      it 'パスワードが全角の場合は登録できない' do
+        @user.password = 'ｐａｓｓｗｏｒｄ１'
+        @user.password_confirmation = 'ｐａｓｓｗｏｒｄ１'
         @user.valid?
         expect(@user.errors.full_messages).to include "Password must contain both letters and numbers"
       end
@@ -67,30 +81,50 @@ RSpec.describe User, type: :model do
 
       it 'お名前(全角)が空では登録できない' do
         @user.lastname_kanji = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Lastname kanji can't be blank"
+      end
+
+      it 'お名前(全角)が空では登録できない' do
         @user.firstname_kanji = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Lastname kanji can't be blank", "Firstname kanji can't be blank"
+        expect(@user.errors.full_messages).to include "Firstname kanji can't be blank"
       end
 
       it 'お名前(全角)が全角（漢字・ひらがな・カタカナ）でない場合は登録できない' do
         @user.lastname_kanji = 'Smith'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Lastname kanji must be full-width (Kanji, Hiragana, Katakana)"
+      end
+
+      it 'お名前(全角)が全角（漢字・ひらがな・カタカナ）でない場合は登録できない' do
         @user.firstname_kanji = 'John'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Lastname kanji must be full-width (Kanji, Hiragana, Katakana)", "Firstname kanji must be full-width (Kanji, Hiragana, Katakana)"
+        expect(@user.errors.full_messages).to include "Firstname kanji must be full-width (Kanji, Hiragana, Katakana)"
       end
 
       it 'お名前カナ(全角)が空では登録できない' do
         @user.lastname_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Lastname kana can't be blank"
+      end
+
+      it 'お名前カナ(全角)が空では登録できない' do
         @user.firstname_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Lastname kana can't be blank", "Firstname kana can't be blank"
+        expect(@user.errors.full_messages).to include "Firstname kana can't be blank"
       end
 
       it 'お名前カナ(全角)が全角（カタカナ）でない場合は登録できない' do
         @user.lastname_kana = 'すみす'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Lastname kana must be full-width Katakana"
+      end
+
+      it 'お名前カナ(全角)が全角（カタカナ）でない場合は登録できない' do
         @user.firstname_kana = 'じょん'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Lastname kana must be full-width Katakana", "Firstname kana must be full-width Katakana"
+        expect(@user.errors.full_messages).to include "Firstname kana must be full-width Katakana"
       end
 
       it '生年月日が空では登録できない' do

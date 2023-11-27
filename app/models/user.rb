5dd78cd4f1599a93c27aaf2226eb3dafd4601d6f
yura1128 <yumeto1128.pr@gmail.com>
2023-11-27 12:27:56 +0900

    ログアウト、新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, :encrypted_password, :firstname_kanji, :lastname_kanji, :firstname_kana, :lastname_kana, :birthday, presence: true
+
+
 end

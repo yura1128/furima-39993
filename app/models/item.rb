16326aa0386a2e6d581a4e3ba60c7a5bcf92680f
yura1128 <yumeto1128.pr@gmail.com>
2023-12-04 15:06:12 +0900

    コメント修正後２

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  has_one :purchase_record
+  #has_one :purchase_record
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :purchase_records
+  #has_many :purchase_records
 
   validates :nickname, :firstname_kanji, :lastname_kanji, :firstname_kana, :lastname_kana, :birthday, presence: true
 
